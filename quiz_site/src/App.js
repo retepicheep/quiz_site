3ef3fab162e4f3b90d414eb8da45962e227173a2
Peter Dillow <peter.dillow@proton.me>
2022-10-01 07:54:34 -0700

    feat: added login screen

--- quiz_site/src/App.js
+++ quiz_site/src/App.js
@@ -1,16 +1,27 @@
 import Header from "./components/Header";
+import Login from "./components/login"
 import './App.css';
 
+let loginSuccessful = false
+
 function App() {
-  return (
-    <div className="App">
-      <Header title = "Quapp">
-        <p>
-          Quapp
-        </p>
-      </Header>
-    </div>
-  );
+  if (loginSuccessful === true){
+    return (
+      <div className="App">
+        <Header title = "Quapp">
+        </Header>
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className = "App">
+        <Header title = "Quapp">
+          <Login/>
+        </Header>
+      </div>
+    );
+  }
 }
 
 export default App;
