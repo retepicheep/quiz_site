ccc8c638174a12730172a7c8debf40d15f1d5524
Peter Dillow <peter.dillow@proton.me>
2022-12-20 10:21:52 -0800

    created quiz app

--- quiz_site/src/App.js
+++ quiz_site/src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Login from "./components/login"
+import Login from "./components/Login"
 import './App.css';
 
 let loginSuccessful = false
@@ -8,17 +8,15 @@ function App() {
   if (loginSuccessful === true){
     return (
       <div className="App">
-        <Header title = "Quapp">
-        </Header>
+        <Header title = "Quapp"/>
       </div>
     );
   }
-  else{
+  if (loginSuccessful === false){
     return (
       <div className = "App">
-        <Header title = "Quapp">
-          <Login/>
-        </Header>
+        <Header title = "Quapp"/>
+        <Login/>
       </div>
     );
   }
--- quiz_site/src/components/Header.js
+++ quiz_site/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Logo alt={title} style={{ maxWidth: "12rem", maxHeight: "12rem" }} />
+        <Logo alt={title} style={{ maxWidth: "12rem", maxHeight: "10rem" }} />
       </Container>
     </Navbar>
   );
